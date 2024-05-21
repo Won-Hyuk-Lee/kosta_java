cb8db11fda5bbdb58092de60b09f837be8047d17
lwh <lwhprt@naver.com>
2024-05-21 17:14:31 +0900

    20240521 add jsp

--- web_prj/src/main/java/com/kosta/sample/board/BoardServlet.java
+++ web_prj/src/main/java/com/kosta/sample/board/BoardServlet.java
@@ -1,6 +1,7 @@
 package com.kosta.sample.board;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,29 +15,27 @@ import javax.servlet.http.HttpServletResponse;
 public class BoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public BoardServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+        //super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.sendRedirect("index.html");
+		response.getWriter().append("GET");
+		System.out.println("====request method : GET====");
+		//response.sendRedirect("IndexPractice.html");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.getWriter().append("POST");
+		
+		String vUserpwd = request.getParameter("userpwd");
+		String vUserid = request.getParameter("userid");
+		String vUsergender = request.getParameter("gender");
+		String vUsercontents = request.getParameter("contents");
+		System.out.println("====request method : POST====");
+		
+		System.out.println(vUserpwd + vUserid + vUsergender + vUsercontents);
 	}
 
 }
